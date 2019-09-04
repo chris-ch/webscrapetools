c5f52d57f293be4cd254f59b53e5b9a46b95f14d
Christophe <ch.alexandre@bluewin.ch>
2019-09-04 21:59:25 +0200

    prepared for dist

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
-from setuptools import setup
-from os import path
+from setuptools import setup, find_packages
 
 __version = '0.4.4'
 
-this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+INSTALL_REQUIRE = ['requests==2.18.1']
 
 setup(
     name='webscrapetools',
     version=__version,
     description='A basic but fast, persistent and threadsafe caching system',
-    long_description=long_description,
+    long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/chris-ch/webscrapetools',
     author='Christophe',
@@ -20,8 +17,6 @@ setup(
     package_dir={'webscrapetools': 'src/webscrapetools'},
     license='Apache',
     download_url='https://github.com/chris-ch/webscrapetools/webscrapetools/archive/{0}.tar.gz'.format(__version),
-    install_requires=[
-        'requests',
-    ],
+    install_requires=INSTALL_REQUIRE,
     zip_safe=True
 )
